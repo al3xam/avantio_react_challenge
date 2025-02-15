5d2e54be1ab85744fb59723714ccb12bb468d597
Alex <amoros.alejandro@gmail.com>
2025-02-15 20:11:14 +0100

    Added tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
+    <div className="bg-gray-900 text-white p-4">
       <h1 className="text-3xl">
         ¡Tailwind CSS está funcionando en TypeScript!
       </h1>
